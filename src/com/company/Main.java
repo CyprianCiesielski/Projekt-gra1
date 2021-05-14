c7ade857900632ee8553d001a67a8ae8652dab81
Agata <agata.k6@onet.pl>
2021-05-14 09:38:40 +0200

    14.05

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-       /*JFrame gra = new JFrame();
+       JFrame gra = new JFrame();
         Panel p = new Panel();
         gra.add(p);
         gra.setIconImage(new ImageIcon("").getImage());
         gra.setTitle("Obraz");
-        gra.setBounds(4,4,1510 + 14, 800 + 14 + 23);
+        gra.setBounds(4,4,800, 800);
         gra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gra.setVisible(true);*/
+        gra.setVisible(true);
 
 
 Scanner sc = new Scanner(System.in);
 
-Przedmiot jedzenie = new Przedmiot("jedzenie",10,20,10,50);
-Przedmiot miecz = new Przedmiot("miecz",0,0,0,0);
-Przedmiot toporek = new Przedmiot("toporek",0,0,0,0);
-Przedmiot opatrunki = new Przedmiot("opatrunki",0,0,0,0);
-Przedmiot zapalki = new Przedmiot("zapałki",0,0,0,0);
+        Przedmiot mieso = new Przedmiot("jablko",10,10,20,50);
+        Przedmiot jablko = new Przedmiot("mieso",10,20,10,20);
+        Przedmiot trutka = new Przedmiot("trutka",-10,-40,-15,5);
+
+        Przedmiot miecz = new Przedmiot("miecz",0,0,0,0);
+        Przedmiot toporek = new Przedmiot("topór",0,0,0,0);
+        Przedmiot tarcza = new Przedmiot("tarcza",0,0,0,0);
+
+        Przedmiot opatrunki = new Przedmiot("opatrunki",10,20,10,0);
+        Przedmiot odtrutka = new Przedmiot("odtrutka",20,30,12,0);
+
 
 System.out.println("Jak masz na imię?");
         Postac postac = new Postac(sc.nextLine());
@@ -40,7 +46,7 @@ System.out.println("Jak masz na imię?");
                     Prawo.p(postac);
                     break;
                 case 2:
-                    Lewo.l(postac, toporek);
+                    Lewo.l(postac, toporek, tarcza, miecz, jablko, mieso, trutka, opatrunki, odtrutka);
                     break;
 
             }
